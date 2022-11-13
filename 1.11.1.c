80f3fbd545bf78856bd72cf072fee7e699291cd9
Purp1ePunk <kirillrogov42@yandex.ru>
2022-11-13 19:43:53 +0700

    11 and instruction

--- 1.11.1.c
+++ 1.11.1.c
@@ -1,4 +1,4 @@
-// Задание 1.11 (2 балла). Сделать две программы: первая передает в именованный
+ // Задание 1.11 (2 балла). Сделать две программы: первая передает в именованный
 // канал случайные числа, а вторая – считывает данные и выводит на экран. Что будет
 // если запустить несколько экземпляров каждой программы?
 
@@ -14,34 +14,40 @@
 
 int main(int argc, char *argv[])
 {
+  srand(time(NULL));
 
   if (argc < 2) {
-    printf("Необходимо передать аргументом количество считываемых чисел\n");
+    printf("Необходимо передать аргументом количество передаваемых чисел\n");
     exit(EXIT_FAILURE);
   }
 
-  int fd_fifo;
   int ready = 0;
+  int fd_fifo;
   int count = strtol(argv[1], NULL, 10); //10 - система счисления
   int arr[count];
 
-  fd_fifo=open("1.fifo", O_RDONLY);
+  unlink("1.fifo");
 
-  if (fd_fifo == -1) {
+  if ((mkfifo("1.fifo", 0666)) == -1) {
+    printf("Невозможно создать FIFO.");
+    exit(EXIT_FAILURE);
+  }
+
+  if ((fd_fifo=open("1.fifo", O_WRONLY)) == -1) {
     printf("Невозможно открыть fifo\n");
     exit(0);
   }
 
-  printf("Введите любое число для чтения:");
+  printf("Введите любое число для записи:");
   scanf("%d", &ready);
-  int size = read(fd_fifo, arr, sizeof(arr));
 
-  if (size < 0) {
-  /* Если прочитать не смогли, сообщаем об ошибке и завершаем работу */
-    printf("Can\'t read string\n");
+  for (int i = 0; i < count; i++) {arr[i] = rand()%10; printf("[ЗАПИСЬ]: %d \n", arr[i]);}
+  int size = write(fd_fifo, &arr, sizeof(arr));
+
+  if (size < sizeof(arr)) {
+    printf("Can\'t write string\n");
     exit(-1);
   }
 
-  for (int i = 0; i < count; i++) {printf("[ЧТЕНИЕ]: %d \n", arr[i]);}
   return 0;
 }
